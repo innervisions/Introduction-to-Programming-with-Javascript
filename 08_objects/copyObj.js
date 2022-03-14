65c194dd1328d8e0c8afe96f9d2719226d437fee
Raymond Saade <raymond.saade@gmail.com>
2022-03-13 22:24:26 -0400

    completed objects

---
+++ 08_objects/copyObj.js
@@ -0,0 +1,37 @@
+// function copyObj(objToCopy, keysToCopy = []) {
+//   if (keysToCopy.length === 0) keysToCopy = Object.keys(objToCopy);
+//   copiedObj = {};
+//   keysToCopy.forEach(function(key) {
+//     copiedObj[key] = objToCopy[key];
+//   });
+//   return copiedObj;
+// }
+
+function copyObj(sourceObject, keys) {
+  let destinationObject = {};
+
+  if (keys) {
+    keys.forEach(function(key) {
+      destinationObject[key] = sourceObject[key];
+    });
+
+    return destinationObject;
+  } else {
+    return Object.assign(destinationObject, sourceObject);
+  }
+}
+
+let objToCopy = {
+  foo: 1,
+  bar: 2,
+  qux: 3,
+};
+
+let newObj = copyObj(objToCopy);
+console.log(newObj);        // => { foo: 1, bar: 2, qux: 3 }
+
+let newObj2 = copyObj(objToCopy, [ 'foo', 'qux' ]);
+console.log(newObj2);       // => { foo: 1, qux: 3 }
+
+let newObj3 = copyObj(objToCopy, [ 'bar' ]);
+console.log(newObj3);       // => { bar: 2 }
---
+++ 08_objects/myArray.js
@@ -0,0 +1,11 @@
+let myArray = {
+  '0': 'Yoshi',
+  '1': 'Vinnie',
+  '2': 'Gibbs',
+  '3': 'Al',
+  'length': 4
+};
+
+for (let i = 0; i < myArray.length; i += 1) {
+  console.log(myArray[i]);
+}
